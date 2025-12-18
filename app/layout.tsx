79be05720214a6047d8354f000cae39b8b939b54
RinLit <lintusup@foxmail.com>
2025-12-19 02:53:05 +0800

    feat: 补全元信息

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
-"use client";
+
 import "./globals.css";
 import "./style/readme.css";
 import {Providers} from "@/app/providers";
 import Header from "@/app/components/Layout/Header";
 import Footer from "@/app/components/Layout/Footer";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "插件广场 - Class Widgets 2",
+  icons: {
+    icon: [
+      {
+        url: "/images/logo.png",
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
